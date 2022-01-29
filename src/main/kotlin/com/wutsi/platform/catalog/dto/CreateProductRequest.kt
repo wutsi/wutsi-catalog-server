707a168313bd9d3e493dfc41a35061c749c99b87
wutsi <herve.tchepannou@wutsi.com>
2022-01-29 16:28:21 -0500

    update contraint

--- src/main/kotlin/com/wutsi/platform/catalog/dto/CreateProductRequest.kt
+++ src/main/kotlin/com/wutsi/platform/catalog/dto/CreateProductRequest.kt
@@ -1,14 +1,17 @@
 package com.wutsi.platform.catalog.dto
 
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 import kotlin.Double
 import kotlin.Long
 import kotlin.String
 
 public data class CreateProductRequest(
     @get:NotBlank
+    @get:Size(max = 100)
     public val title: String = "",
     @get:NotBlank
+    @get:Size(max = 160)
     public val summary: String = "",
     public val description: String? = null,
     public val categoryId: Long? = null,

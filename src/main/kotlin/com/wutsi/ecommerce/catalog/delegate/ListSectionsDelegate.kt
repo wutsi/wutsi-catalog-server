6f2df530f604b981f15d5f1aa61ee4f768178acc
wutsi <herve.tchepannou@wutsi.com>
2022-04-11 07:24:29 -0400

    add more logging

--- src/main/kotlin/com/wutsi/ecommerce/catalog/delegate/ListSectionsDelegate.kt
+++ src/main/kotlin/com/wutsi/ecommerce/catalog/delegate/ListSectionsDelegate.kt
@@ -1,14 +1,19 @@
 package com.wutsi.ecommerce.catalog.`delegate`
 
 import com.wutsi.ecommerce.catalog.dto.ListSectionResponse
+import com.wutsi.platform.core.logging.KVLogger
 import org.springframework.stereotype.Service
 
 @Service
-class ListSectionsDelegate : AbstractSectionDelegate() {
-    fun invoke(accountId: Long) = ListSectionResponse(
-        sections = dao.findByAccountId(accountId)
+class ListSectionsDelegate(private val logger: KVLogger) : AbstractSectionDelegate() {
+    fun invoke(accountId: Long): ListSectionResponse {
+        val sections = dao.findByAccountId(accountId)
             .filter { !it.isDeleted }
             .sortedBy { it.sortOrder }
-            .map { it.toSectionSummary() }
-    )
+
+        logger.add("count", sections.size)
+        return ListSectionResponse(
+            sections = sections.map { it.toSectionSummary() }
+        )
+    }
 }

c331f1d521e1a68caaf1928d4a7fd3cd904cdf12
wutsi <herve@wutsi.com>
2022-05-09 13:42:30 -0400

    update description

--- src/main/kotlin/com/wutsi/ecommerce/catalog/service/facebook/FacebookMapper.kt
+++ src/main/kotlin/com/wutsi/ecommerce/catalog/service/facebook/FacebookMapper.kt
@@ -11,9 +11,8 @@ import java.util.Locale
 class FacebookMapper {
     fun toFacebookProduct(product: ProductEntity, account: Account, tenant: Tenant) = FacebookProduct(
         id = product.id.toString(),
-        title = product.title.lowercase()
-            .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() },
-        description = toString(product.summary) ?: product.title,
+        title = capitalize(product.title),
+        description = toString(product.summary) ?: capitalize(product.title),
         availability = if (product.quantity > 0) "in stock" else "out of stock",
         condition = "new",
         price = formatMoney(product.comparablePrice ?: product.price!!, tenant),
@@ -27,6 +26,10 @@ class FacebookMapper {
             .map { it.url }
             .take(20)
     )
+
+    private fun capitalize(value: String): String =
+        value.lowercase()
+            .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
 
     private fun formatMoney(amount: Double, tenant: Tenant): String =
         DecimalFormat(tenant.numberFormat).format(amount) + " ${tenant.currency}"

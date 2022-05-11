8e6c4a040de70db5058fa2b049c24543bd71e290
wutsi <herve@wutsi.com>
2022-05-10 22:57:38 -0400

    update csv

--- src/main/kotlin/com/wutsi/ecommerce/catalog/delegate/FacebookFeedDelegate.kt
+++ src/main/kotlin/com/wutsi/ecommerce/catalog/delegate/FacebookFeedDelegate.kt
@@ -10,7 +10,6 @@ import com.wutsi.platform.account.dto.Account
 import com.wutsi.platform.account.entity.AccountStatus
 import com.wutsi.platform.core.logging.KVLogger
 import com.wutsi.platform.tenant.WutsiTenantApi
-import com.wutsi.platform.tenant.dto.Tenant
 import org.springframework.stereotype.Service
 import javax.servlet.http.HttpServletResponse
 
@@ -25,56 +24,38 @@ class FacebookFeedDelegate(
 ) {
     fun invoke(accountId: Long) {
         val account = accountApi.getAccount(accountId).account
-
         logger.add("account_id", accountId)
         logger.add("account_status", account.status)
-        logger.add("account_display_name", account.displayName)
 
-        if (account.status.equals(AccountStatus.ACTIVE.name, true))
-            invoke(account)
+        val products = if (account.status.equals(AccountStatus.ACTIVE.name, true))
+            load(account)
         else
-            write(emptyList())
+            emptyList()
+
+        logger.add("product_count", products.size)
+        write(products)
     }
 
-    private fun invoke(account: Account) {
-        val limit = 100
-        var offset = 0
-        val fbProducts = mutableListOf<FacebookProduct>()
-        var tenant: Tenant? = null
+    private fun load(account: Account): List<FacebookProduct> {
+        // Product
+        val products = search.search(
+            request = SearchProductRequest(
+                limit = 100,
+                offset = 0,
+                accountId = account.id,
+                status = ProductStatus.PUBLISHED.name,
+            ),
+            tenantId = null
+        )
+        if (products.isEmpty())
+            return emptyList()
 
-        while (true) {
-            // Product
-            val products = search.search(
-                request = SearchProductRequest(
-                    limit = limit,
-                    offset = offset,
-                    accountId = account.id,
-                    status = ProductStatus.PUBLISHED.name,
-                ),
-                tenantId = null
-            )
-            if (products.isEmpty())
-                break
-
-            // Tenant
-            if (tenant == null)
-                tenant = tenantApi.getTenant(products[0].tenantId).tenant
-
-            // Map
-            fbProducts.addAll(products.map { mapper.toFacebookProduct(it, account, tenant) })
-
-            // Next
-            if (products.size < limit)
-                break
-            else
-                offset += limit
-        }
-
-        logger.add("product_count", fbProducts.size)
-        write(fbProducts)
+        // Map
+        val tenant = tenantApi.getTenant(products[0].tenantId).tenant
+        return products.map { mapper.toFacebookProduct(it, account, tenant) }
     }
 
-    private fun write(fbProducts: List<FacebookProduct>) {
-        FacebookWriter().write(fbProducts, response.outputStream)
+    private fun write(products: List<FacebookProduct>) {
+        FacebookWriter().write(products, response.outputStream)
     }
 }
